028b0f779ce01d6b07cfbfb2a5954b2f1f37cb3b
J. Duron <Jduroncortiz@gmail.com>
2023-03-20 13:12:08 +0100

    SMS jobs

--- readSMS.py
+++ readSMS.py
@@ -3,18 +3,18 @@ from time import sleep
 
 
 def read_SMSValue():
-
+    adc = MCP3008(channel=0)
     SMS_voltage = adc.value * 3.3
     SMS_percentage = (SMS_voltage / 3.3) * 100
     return SMS_percentage
 
-def converter(gen):
-    for value in gen:
-        yield ((1-value) * 100)
+#def converter(gen):
+#    for value in gen:
+#        yield ((1-value) * 100)
 
-adc = MCP3008(channel=0)
+
 #SMS = MCP3008(channel=0, clock_pin=17, mosi_pin=18, miso_pin=27)
 
-for data in converter(adc.values):
-    print('Soil moisture: ', data, '%')
-    sleep(1)
+#for data in converter(adc.values):
+#    print('Soil moisture: ', data, '%')
+#    sleep(1)
