8c5e90753c0216bab6d4c2752942bfef11fa7aa8
FLORIAN <florian.delplancke@student.kuleuven.be>
2023-03-20 11:58:10 +0100

    DB connectie gefixt hoop ik ouwe

--- connectDB.py
+++ connectDB.py
@@ -19,7 +19,6 @@ def pushtoDB(LDR, AHS, SMS, NTC):
 
 # readSMS()
 
-while True:
     # temperatureVal = random.uniform(20, 30)
     # humidityVal = random.randon()
     # moistVal = random.randon()
@@ -30,16 +29,16 @@ while True:
     # newRowId = int(mycursor.fetchall()[0][0]) + 1
 
     # print(newRowId)
-    currentDate = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    currentDateDate = datetime.datetime.strptime(currentDate, "%Y-%m-%d %H:%M:%S")
-    print(currentDateDate)
+    # currentDate = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # currentDateDate = datetime.datetime.strptime(currentDate, "%Y-%m-%d %H:%M:%S")
+    # print(currentDateDate)
 
     # ntcArray.append(temperatureVal)
     # print("temperature registered: [" + str(temperatureVal) + "]")
     # time.sleep(5)
 
     # insert = "INSERT INTO a22ib2a03.measurements (ID,temperature, time) VALUES (('%s', '%s','%s'),(" + str(newRowId) +","+ str(temperatureVal) +" , " + str(currentDateDate) +"))"  # str(currentDate)
-    insert = "INSERT INTO a22ib2a03.measurements (temperature, humidity, moisture,light, time) VALUES ( " + str(temperatureVal) +", "+ str(humidityVal) +", " + str(moistVal) +"," + str(lightVal) + " , CURRENT_TIMESTAMP)"
+    insert = "INSERT INTO a22ib2a03.measurements (temperature, humidity, moisture,light, time) VALUES ( " + str(NTC) +", "+ str(AHS) +", " + str(SMS) +"," + str(LDR) + " , CURRENT_TIMESTAMP)"
     mycursor.execute(insert)
 
     # Example query
@@ -47,10 +46,14 @@ while True:
     database.commit()
 
     # Fetch the results
-    myresult = mycursor.fetchall()
+    # myresult = mycursor.fetchall()
+
+    # close the cursor and connection
+    mycursor.close()
+    database.close()
 
     # Print the results
-    for row in myresult:
-        print(row)
+    # for row in myresult:
+    #     print(row)
 
 
