8e6d5b6dcac1d29d6f54630d81f882fc937ff34f
FLORIAN <florian.delplancke@student.kuleuven.be>
2023-03-20 11:13:25 +0100

    nieuwe main file

---
+++ main.py
@@ -0,0 +1,22 @@
+import readSMS
+import readAHS
+import readDB
+import readLDR
+import readNTC
+
+
+
+while True:
+
+    #read the outputs DataBase
+
+    # do some enable/disable logic
+
+    # enable/ disable the pump, led, fan
+
+    # read sensors
+    readAHS.converter()
+
+    # upload sensor data
+
+
