6dd1b2d346c50b1e0ff4304ec0b5c35d0c33aa00
Matthieu Loix <matthieu.loix@student.kuleuven.be>
2023-03-20 11:20:22 +0100

    All sensors to DB

--- connectDB.py
+++ connectDB.py
@@ -20,6 +20,9 @@ readSMS()
 
 while True:
     temperatureVal = random.uniform(20, 30)
+    humidityVal = random.randon()
+    moistVal = random.randon()
+    lightVal = random.random()
     getId = "SELECT COUNT(ID) FROM measurements"
     #print(rows)
     mycursor.execute(getId)
@@ -35,7 +38,7 @@ while True:
     time.sleep(5)
 
     # insert = "INSERT INTO a22ib2a03.measurements (ID,temperature, time) VALUES (('%s', '%s','%s'),(" + str(newRowId) +","+ str(temperatureVal) +" , " + str(currentDateDate) +"))"  # str(currentDate)
-    insert = "INSERT INTO a22ib2a03.measurements (ID,temperature, time) VALUES ( " + str(newRowId) +","+ str(temperatureVal) +" , CURRENT_TIMESTAMP)"
+    insert = "INSERT INTO a22ib2a03.measurements (temperature, humidity, moisture,light, time) VALUES ( " + str(temperatureVal) +", "+ str(humidityVal) +", " + str(moistVal) +"," + str(lightVal) + " , CURRENT_TIMESTAMP)"
     mycursor.execute(insert)
 
     # Example query
